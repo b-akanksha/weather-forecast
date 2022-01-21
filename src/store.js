1a4026d430ec6dabd5d83e551018c22db276c50e
akanksha <b.akanksha6@gmail.com>
2022-01-21 11:54:37 +0530

    redux logs

--- src/store.js
+++ src/store.js
@@ -1,16 +1,18 @@
 import { applyMiddleware, createStore, combineReducers, compose } from "redux";
-import thunk from 'redux-thunk';
-import { logger } from 'redux-logger';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from "redux-thunk";
+import { logger } from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 import { weatherReducer } from "./components/weatherApp/redux/reducer";
 
 const rootReducer = combineReducers({ weather: weatherReducer });
-const middlewares = [thunk, logger];
-
+const middlewares =
+  process.env.NODE_ENV === "production" ? [thunk] : [thunk, logger];
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(...middlewares))
+  process.env.NODE_ENV === "production"
+    ? compose(applyMiddleware(...middlewares))
+    : composeWithDevTools(applyMiddleware(...middlewares))
 );
 
 export default store;
